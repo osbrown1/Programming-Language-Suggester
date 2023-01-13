290d248874ec51df0efc83ae6dc39d749c1bfa42
osbrown1 <socknot@live.com>
2023-01-13 14:14:00 -0800

    still working on getting button to display output

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,20 @@
-document.getElementById("submit-button").addEventListener("click", function() {
-  // Get user's answers to the survey questions
-  let question1 = document.getElementById("question1").value;
-  let question2 = document.getElementById("question2").value;
-  let question3 = document.getElementById("question3").value;
-  let question4 = document.getElementById("question4").value;
-  let question5 = document.getElementById("question5").value;
+onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
 
-  // Initialize a variable to store the suggested language
-  let suggestedLanguage;
 
-  // Use branching logic to determine the suggested language based on the user's answers
-  if (question1 === "web") {
-    suggestedLanguage = "JavaScript";
-  } else if (question1 === "mobile") {
-    suggestedLanguage = "Swift";
-  } else if (question1 === "desktop") {
-    suggestedLanguage = "C#";
-  } else {
-    suggestedLanguage = "Python";
-  }
+  // determine language suggestion
+let suggestion = "";
+if (question1 === "beginner" && question2 === "web" && question3 === "object-oriented") {
+  suggestion = "JavaScript";
+} else if (question1 === "intermediate" && question2 === "desktop" && question3 === "functional") {
+  suggestion = "Python";
+} else if (question1 === "expert" && question2 === "mobile" && question3 === "procedural") {
+  suggestion = "C++";
+} else {
+  suggestion = "Sorry, we could not suggest a language based on your answers.";
+}
+};
 
-  // Display the suggested language to the user
-  let resultDiv = document.getElementById("result");
-  resultDiv.innerHTML = "Based on your answers, we suggest you learn " + suggestedLanguage + " first.";
-});
+};
