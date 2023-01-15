c0d5e0fddeaa58173c8befa60a02d0e914753c8e
osbrown1 <socknot@live.com>
2023-01-15 14:03:54 -0800

    added comments and put bracket in correct place to implement branching in scripts.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,38 @@
+// function handleSelect(event) {
+//   // event.preventDefault();
+//   const form = document.getElementById("survey-form");
+//   const result = document.getElementById("result");
+//   const experience = document.getElementById("experience");
+//   const goal = document.getElementById("goal");
+//   const platform = document.getElementById("platform");
+//   const color = document.getElementById("color");
+//   const speed = document.getElementById("speed");
+//   let suggestion = "";
+//   if (experience.value === "none" && goal.value === "web-development") {
+//     suggestion = "JavaScript";
+//   } else if (goal.value === "data-science" && color.value === "green") {
+//     suggestion = "Python";
+//   } else if (platform.value === "desktop" && speed.value === "super important") {
+//     suggestion = "C++";
+//   } else if (goal.value === "money" && color.value === "blue") {
+//     suggestion = "Java";
+//   } else {
+//     suggestion = "No language suggestion";
+//   }
+//   console.log(suggestion);
+// }
+
+//this function gets the elements on button press 
+//and then provides a suggestion
 function handleSelect(event) {
   event.preventDefault();
-const form = document.getElementById("survey-form");
-const result = document.getElementById("result");
-const experience = document.getElementById("experience");
-const goal = document.getElementById("goal");
-const platform = document.getElementById("platform");
-const color = document.getElementById("color");
-const speed = document.getElementById("speed");
-}
-  
+  const form = document.getElementById("survey-form");
+  const result = document.getElementById("result");
+  const experience = document.getElementById("experience");
+  const goal = document.getElementById("goal");
+  const platform = document.getElementById("platform");
+  const color = document.getElementById("color");
+  const speed = document.getElementById("speed");
   let suggestion = "";
   if (experience.value === "none" && goal.value === "web-development") {
     suggestion = "JavaScript";
@@ -21,8 +45,18 @@ const speed = document.getElementById("speed");
   } else {
     suggestion = "No language suggestion";
   }
+  console.log(suggestion); //TODO replace with update the HTML string
+}
+  
+  //make sure the elements are loaded before adding listener
+  document.addEventListener('DOMContentLoaded', function () {
+    var button = document.getElementById("my_button");
+    button.addEventListener("click", handleSelect);
+  });
 
-  addEventListener("load", function() {
-  document.getElementById("survey-form").addEventListener("submit", handleSelect);
-});
+  
+
+  // button.addEventListener("load", function() {
+  //   document.getElementById("survey-form").addEventListener("submit", handleSelect);
+  // });
 
