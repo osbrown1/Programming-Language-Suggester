ce80e8970a383e35f5a3457d547b3c07bcd5091f
osbrown1 <socknot@live.com>
2023-01-16 13:31:49 -0800

    Can get output on button click to show up in dev tools console with console.log, but not the webpage itself.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,3 @@
-// function handleSelect(event) {
-//   // event.preventDefault();
-//   const form = document.getElementById("survey-form");
-//   const result = document.getElementById("result");
-//   const experience = document.getElementById("experience");
-//   const goal = document.getElementById("goal");
-//   const platform = document.getElementById("platform");
-//   const color = document.getElementById("color");
-//   const speed = document.getElementById("speed");
-//   let suggestion = "";
-//   if (experience.value === "none" && goal.value === "web-development") {
-//     suggestion = "JavaScript";
-//   } else if (goal.value === "data-science" && color.value === "green") {
-//     suggestion = "Python";
-//   } else if (platform.value === "desktop" && speed.value === "super important") {
-//     suggestion = "C++";
-//   } else if (goal.value === "money" && color.value === "blue") {
-//     suggestion = "Java";
-//   } else {
-//     suggestion = "No language suggestion";
-//   }
-//   console.log(suggestion);
-// }
-
 //this function gets the elements on button press 
 //and then provides a suggestion
 function handleSelect(event) {
@@ -43,14 +19,14 @@ function handleSelect(event) {
   } else if (goal.value === "money" && color.value === "blue") {
     suggestion = "Java";
   } else {
-    suggestion = "No language suggestion";
+    suggestion = "Hmm try different options";
   }
-  console.log(suggestion); //TODO replace with update the HTML string
+  (suggestion); //TODO replace with update the HTML string
 }
   
   //make sure the elements are loaded before adding listener
   document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById("my_button");
+    let button = document.getElementById("my_button");
     button.addEventListener("click", handleSelect);
   });
 
